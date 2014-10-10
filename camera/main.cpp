a314ecd68f10be9d55eea3e7a9684676a21cf546
mike-kang <ksflex@gmail.com>
2014-10-10 01:14:32 +0000

    update testcode

--- camera/main.cpp
+++ camera/main.cpp
@@ -1,12 +1,26 @@
 #include "camerastill.h"
 #include <iostream>
+#include <fstream>
 
+using namespace std;
 int main()
 {
   CameraStill* m_cameraStill;
 
-  m_cameraStill = new CameraStill(10); //10 minutes
-  m_cameraStill->takePicture();
+  m_cameraStill = new CameraStill(10); //10 sec
+  char* imgBuf = NULL;;
+  int imgLength;
+  if(m_cameraStill->takePicture(&imgBuf, &imgLength, 2))  //2sec 
+  {
+    //for test
+    if(imgLength < 0)
+      cout << "takePicture error!" << endl;
+    else{
+      ofstream outfile ("test.jpg",ofstream::binary);
+      outfile.write (imgBuf,imgLength);
+      outfile.close();
+    }
+  }
 
   while(1)
     sleep(1);
