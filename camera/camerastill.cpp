c50c2c9173d1c155210012a7ef794eeb79eddb9f
mike-kang <ksflex@gmail.com>
2014-11-08 16:51:00 +0900

    add camera exception

--- camera/camerastill.cpp
+++ camera/camerastill.cpp
@@ -86,7 +86,10 @@ bool CameraStill::takePicture(char** buf, int* len, int maxWaitTime)
   mtx.lock();
   if(m_timer->IsActive())
     m_timer->stop();
-  takePictureReady();
+  if(!takePictureReady()){
+    LOGE("takePictureReady fail!\n");
+    return false;
+  }
   m_camera_component->capture(true);
   m_encoder_component->capture();
   mtx.unlock();
@@ -127,39 +130,45 @@ void CameraStill::cbEndOfStream(void* clientData)
 bool CameraStill::takePictureReady()
 {
   if(m_state == IREADY_S) return true;
-  
-  m_camera_component = new OMXILCameraComponent(CAM_WIDTH, CAM_HEIGHT);
-  m_camera_component->load_camera_drivers();
-  
-  m_camera_component->change_state(OMX_StateIdle); //blocking
 
-  m_camera_component->configure_sensor_settings();
-  m_camera_component->set_output_port();
-  m_camera_component->set_camera_settings();
-
-  //Setup tunnels: camera (still) -> image_encode, camera (preview) -> null_sink
-  if(!OMXILComponent::connect(m_camera_component, OMXILCameraComponent::OUTPUT_CAPTURE_PORT_NUM, m_encoder_component, OMXILEncoderComponent::INPUT_PORT_NUM)){
+  try{
+    m_camera_component = new OMXILCameraComponent(CAM_WIDTH, CAM_HEIGHT);
+    m_camera_component->load_camera_drivers();
+    
+    m_camera_component->change_state(OMX_StateIdle); //blocking
+    
+    m_camera_component->configure_sensor_settings();
+    m_camera_component->set_output_port();
+    m_camera_component->set_camera_settings();
+    
+    //Setup tunnels: camera (still) -> image_encode, camera (preview) -> null_sink
+    if(!OMXILComponent::connect(m_camera_component, OMXILCameraComponent::OUTPUT_CAPTURE_PORT_NUM, m_encoder_component, OMXILEncoderComponent::INPUT_PORT_NUM)){
+      return false;
+    }
+    if(!OMXILComponent::connect(m_camera_component, OMXILCameraComponent::OUTPUT_PREVIEW_PORT_NUM, m_preview_component, 240)){
+      return false;
+    }
+    
+    //Enable the tunnel ports
+    m_camera_component->enable_port (OMXILCameraComponent::OUTPUT_CAPTURE_PORT_NUM);
+    m_camera_component->enable_port (OMXILCameraComponent::OUTPUT_PREVIEW_PORT_NUM);
+    m_preview_component->enable_port (240);
+    m_encoder_component->enable_port (OMXILEncoderComponent::INPUT_PORT_NUM);
+    m_encoder_component->enable_port (OMXILEncoderComponent::OUTPUT_PORT_NUM);
+    
+    LOGV("Change state to EXECUTING\n");
+    //Change state to EXECUTING
+    m_camera_component->change_state(OMX_StateExecuting); //blocking
+    m_preview_component->change_state(OMX_StateExecuting); //blocking
+    m_encoder_component->change_state(OMX_StateExecuting); //blocking
+    
+    m_state = IREADY_S;
+    return true;
+  }
+  catch(OMXILComponent::Except e){
     return false;
   }
-  if(!OMXILComponent::connect(m_camera_component, OMXILCameraComponent::OUTPUT_PREVIEW_PORT_NUM, m_preview_component, 240)){
-    return false;
-  }
-
-  //Enable the tunnel ports
-  m_camera_component->enable_port (OMXILCameraComponent::OUTPUT_CAPTURE_PORT_NUM);
-  m_camera_component->enable_port (OMXILCameraComponent::OUTPUT_PREVIEW_PORT_NUM);
-  m_preview_component->enable_port (240);
-  m_encoder_component->enable_port (OMXILEncoderComponent::INPUT_PORT_NUM);
-  m_encoder_component->enable_port (OMXILEncoderComponent::OUTPUT_PORT_NUM);
-
-  LOGV("Change state to EXECUTING\n");
-  //Change state to EXECUTING
-  m_camera_component->change_state(OMX_StateExecuting); //blocking
-  m_preview_component->change_state(OMX_StateExecuting); //blocking
-  m_encoder_component->change_state(OMX_StateExecuting); //blocking
-
-  m_state = IREADY_S;
-  return true;
+  
 }
 
 void CameraStill::returnResources()
