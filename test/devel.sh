e1f091d9a95c31c2ef1b71659029869bd71c1fee
mikekang <mikekang@mikekang-virtual-machine.(none)>
2015-06-28 07:34:18 +0900

    update test

--- test/devel.sh
+++ test/devel.sh
@@ -6,5 +6,5 @@ read -p "Do you use mtrace? (y/n)" ret
 if [ "$ret" == "y" ]; then
   MALLOC_TRACE=mtrace.log  LD_LIBRARY_PATH=${LIB_PATH}:${LIB_PATH}/tools/:${LIB_PATH}/inih_r29/ ${_PATH}/main -c FID.test.ini
 else
-  LD_LIBRARY_PATH=${LIB_PATH}:${LIB_PATH}/tools/:${LIB_PATH}/inih_r29/ ${_PATH}/acu -c FID.test.ini
+  LD_LIBRARY_PATH=${LIB_PATH}:${LIB_PATH}/tools/:${LIB_PATH}/inih_r29/ ${_PATH}/main -c FID.test.ini
 fi  
