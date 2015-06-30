c2fdadf39ec7a828be795313e9bb9b5c392dd3ae
mike-kang <ksflex@gmail.com>
2015-06-30 23:47:05 +0900

    add ulimit core dump setting

--- qt/acu/start_sa.sh
+++ qt/acu/start_sa.sh
@@ -1,4 +1,12 @@
 _PATH=/home/pi/acu
 LIB_PATH=${_PATH}/libs/
 
-LD_LIBRARY_PATH=${LIB_PATH}:${LIB_PATH}/tools/:${LIB_PATH}/camera/:${LIB_PATH}/inih_r29/ xinit ${_PATH}/acu $* -- :1
+ulimit -c unlimited
+
+while [ 1 ]
+do
+  LD_LIBRARY_PATH=${LIB_PATH}:${LIB_PATH}/tools/:${LIB_PATH}/camera/:${LIB_PATH}/inih_r29/ xinit ${_PATH}/acu $* -- :1
+
+  sleep 2
+
+done
