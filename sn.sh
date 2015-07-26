145479fbdc1987b0790c6564f7741b6963c0ab3a
mike-kang <ksflex@gmail.com>
2015-07-26 23:10:18 +0900

    add stress test script

---
+++ sn.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "sn $1" > /tmp/cmd
+cat /tmp/response
+
+
---
+++ sn_repeat.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+while [ 1 ]
+do
+  ./sn.sh 212082123175
+  sleep 10
+  ./sn.sh 164175228070
+  sleep 10
+  ./sn.sh 132124211092
+  sleep 10
+done
